9bd930dd4ddbd7a3043c5f6e8faa78dbd9f5788a
alexforce2 <alexforce2@gmail.com>
2021-04-06 00:31:54 +0500

    Исправления

--- app/Rules/Schedule/IsDateAvailable.php
+++ app/Rules/Schedule/IsDateAvailable.php
@@ -7,11 +7,21 @@ use Illuminate\Contracts\Validation\Rule;
 
 class IsDateAvailable implements Rule
 {
-    public function passes($attribute, $value): bool
+    private ?string $endAt;
+
+    public function passes($attribute, $startAt): bool
     {
-        $busy = Schedule::where('start_at', '<=', $value)
-           ->orWhere('end_at', '>=', $value)
-           ->count();
+        $busy = Schedule::where(function ($query) use ($startAt) {
+            $query->where('start_at', '<=', $startAt)
+                ->where('end_at', '>=', $startAt);
+        })
+            ->orWhere(function ($query) use ($startAt) {
+                $query->where('start_at', '>=', $startAt)
+                    ->where('start_at', '<=', $this->endAt);
+            })
+            ->count();
+
+        //в идеале проверку 'end_at' надо вынести в отдельное правило, что бы ошибка отображалась в нужном поле)
 
         return $busy > 0 ? false : true;
     }
@@ -19,5 +29,12 @@ class IsDateAvailable implements Rule
     public function message(): string
     {
         return 'This date ":attribute" has already been booked.';
+    }
+
+    public function setEndDate(string $endAt = null): self
+    {
+        $this->endAt = $endAt;
+
+        return $this;
     }
 }
